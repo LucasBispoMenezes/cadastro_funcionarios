d5a910758698c050708e5b8878dcf297a767979c
Lucas Bispo <skaliboo1@gmail.com>
2022-11-10 17:39:16 -0300

    fix: I specified what I wanted in return

--- backend/src/service/Usuario.ts
+++ backend/src/service/Usuario.ts
@@ -1,14 +1,21 @@
 import prisma from "../database/prismaConnection"
 import 'express-async-errors'
+import { ErrorApi } from "../classes/classeError"
 
 const findUniqueUser = async (username: string, password: string) => {
 	const  user = await prisma.usuario.findFirst({
 		where:{
 			username,
 			senha: password
-		}
-	})
-	if(!user) throw new Error("Usuário não encontrado")
+		},
+	select:{
+		id: true,
+		username:true,
+		role:true,
+	}})
+	
+	if(!user) throw new ErrorApi("Usuário não encontrado", 404)
+	if(!user.id) throw new ErrorApi("local não autorizado", 403)
 	return user
 }
 
