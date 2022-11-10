eccd728c8560286ab88589148078f1b38aee6eb4
Lucas Bispo <skaliboo1@gmail.com>
2022-11-10 17:38:26 -0300

    fix: add validations in controller, add return token

--- backend/src/controllers/Usuario.ts
+++ backend/src/controllers/Usuario.ts
@@ -1,12 +1,20 @@
-import { Request, Response } from "express"
-import findUniqueUser from '../service/Usuario';
-import 'express-async-errors'
+import { Request, Response } from "express";
+import findUniqueUser from "../service/Usuario";
+import "express-async-errors";
+import { loginSchema } from "../validations/schema/login";
+import { ErrorApi } from "../classes/classeError";
+import { generateToken } from "../auth/index";
 
 const getUser = async (req: Request, res: Response) => {
-	console.log(req.body);
 	const { username, password } = req.body;
-	const user = await findUniqueUser(username, password);
-	return res.status(200).json(user); 
-}
+	const { error } = loginSchema.validate(req.body);
 
-export default getUser
+	if (error) throw new ErrorApi(error.message, 400);
+
+	const user = await findUniqueUser(username, password);
+	const token = generateToken(user);
+
+	return res.status(200).json({ token });
+};
+
+export default getUser;
