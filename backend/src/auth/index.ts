8890961ee8089d8041abdb70d0274dbc97281ce9
Lucas Bispo <skaliboo1@gmail.com>
2022-11-10 17:37:20 -0300

    feat: add jwt

---
+++ backend/src/auth/index.ts
@@ -0,0 +1,18 @@
+import  jwt from "jsonwebtoken"
+import "dotenv/config"
+
+interface IToken {
+	id: number,
+	username: string,
+	role: "admin" | "user" 
+}
+
+export const generateToken = (user: IToken): string=> {
+	const token = jwt.sign({user}, process.env.SECRET as string, {expiresIn: '1d', algorithm: "HS256"})
+	return token
+}
+
+export const verifyToken = (token: string): IToken => {
+	const {user} = jwt.verify(token, process.env.SECRET as string) as {user: IToken}
+	return user
+}
