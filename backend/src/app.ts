f66afeb29e9fef8cf964aa3b6faef54fa819c2f9
Lucas Bispo <skaliboo1@gmail.com>
2022-11-11 16:03:22 -0300

    feat: add cors in app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import { midErro } from "./middleware/middleError";
 import rUsuario from "./routes/Usuario";
-
+import cors from "cors";
 const app = express();
 app.use(express.json());
-
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+	})
+);
 
 app.use("/usuario", rUsuario);
-app.use(midErro)
+app.use(midErro);
 export default app;
