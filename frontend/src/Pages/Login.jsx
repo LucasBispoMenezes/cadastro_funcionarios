de5ff5abb9d2bf5c950caa0e1de321634945a582
Lucas Bispo <skaliboo1@gmail.com>
2022-11-11 16:08:45 -0300

    create pages login

---
+++ frontend/src/Pages/Login.jsx
@@ -0,0 +1,60 @@
+import { useForm } from "react-hook-form";
+import React from "react";
+import { requestLogin } from "../helpers/requests";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { setToken } from "../helpers/localStorage";
+import { useNavigate } from "react-router-dom";
+
+export const Login = () => {
+    const navigate = useNavigate();
+    const onSubmit = async (value) => {
+        try {
+            const { data } = await toast.promise(
+                requestLogin("/usuario/login", value),
+                {
+                    pending: "Carregando...",
+                    success: "Bem Vindo",
+                }
+            );
+            await setToken(data.token);
+            setTimeout(() => {
+                navigate("/admin");
+            }, 2000);
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    };
+
+    const {
+        handleSubmit,
+        register,
+        formState: { isDirty, isValid },
+    } = useForm();
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} method="post">
+            <ToastContainer />
+            <label htmlFor="username-input">
+                Usuario
+                <input
+                    type="text"
+                    id="username-input"
+                    {...register("username", {
+                        required: "Usuario Requerido",
+                        minLength: 3,
+                    })}
+                />
+            </label>
+            <label htmlFor="password-input">
+                Senha
+                <input
+                    type="password"
+                    name=""
+                    id="password-input"
+                    {...register("password")}
+                />
+            </label>
+            <input type="submit" disabled={!isDirty || !isValid} />
+        </form>
+    );
+};
