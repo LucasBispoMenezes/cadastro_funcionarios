7a889f665a1adbf8a7eccddcdb5b42aae44be904
Lucas Bispo <skaliboo1@gmail.com>
2022-11-11 16:09:10 -0300

    init pages admin

---
+++ frontend/src/Pages/Admin.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export const Admin = () => {
+    return (
+        <nav>
+            <Link to="/admin/Cadastro">Users</Link>
+        </nav>
+    )
+};
