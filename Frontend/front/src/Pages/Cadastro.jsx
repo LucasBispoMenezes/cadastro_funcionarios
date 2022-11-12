00242c08cbd6f8eb2f111b52b96e1eec1c263397
Lucas Bispo <skaliboo1@gmail.com>
2022-11-12 16:12:26 -0300

    create new step

--- Frontend/front/src/Pages/Cadastro.jsx
+++ Frontend/front/src/Pages/Cadastro.jsx
@@ -4,8 +4,7 @@ import { isAuthenticated } from "../helpers/isAuthenticated";
 // import StepZilla from "react-stepzilla";
 import Pessoa from "./pessoa";
 import { Login } from "./Login";
-import MultiStep, {
-} from "@armandoroman1016/react-multi-step-form";
+import MultiStep from "@armandoroman1016/react-multi-step-form";
 
 function Cadastro() {
     const navigate = useNavigate();
@@ -16,7 +15,12 @@ function Cadastro() {
 
     const steps = [
         { name: "Cadastro de Pessoa", component: Pessoa },
-        { name: "login", component: Login },
+        { name: "Endereços", component: Login },
+        { name: "Telefone", component: Login },
+        { name: "Conta bancaria", component: Login },
+        { name: "Documentos", component: Login },
+        { name: "Relacionameto", component: Login },
+        { name: "Usuario", component: Login },        
     ];
     return <MultiStep steps={steps}></MultiStep>;
 }
--- Frontend/front/src/context/cadastroProvider.js
+++ Frontend/front/src/context/cadastroProvider.js
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
-import  cadastroContext from './contextCadastros';
+import cadastroContext from './contextCadastros';
 
 export default function DataValueConcat({ children }) {
-  const [data, setData] = useState([]);
+    const [data, setData] = useState([{}]);
 
-  function addNewData(obj) {
-    setData(
-       ...data,
-       obj
+    function addNewData(obj) {
+        setData(
+            [
+                ...data,
+                obj
+            ]
+        );
+    }
+
+    const total = {
+        data,
+        addNewData
+    }
+    return (
+        <cadastroContext.Provider value={total}>
+            <div>
+                {children}
+            </div>
+        </cadastroContext.Provider>
     );
-  }
-  
-const total ={
-    data,
-   addNewData 
-}
-  return (
-    <cadastroContext.Provider value={total}>
-      <div>
-        { children }
-      </div>
-    </cadastroContext.Provider>
-  );
 }
