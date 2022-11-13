69393d0f559cd925a9afd7177e24708b899b2a1b
Lucas Bispo <skaliboo1@gmail.com>
2022-11-13 17:00:13 -0300

    create tela conta bancaria

--- Frontend/front/src/Pages/ContaBamcaria.jsx
+++ Frontend/front/src/Pages/ContaBamcaria.jsx
@@ -1,5 +1,145 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-use-before-define */
+import React, { useState, useContext, useEffect } from "react";
+import cadastroContext from "../context/contextCadastros";
+import { useForm, useFieldArray, useWatch } from "react-hook-form";
 function Conta() {
-    return <h1>conta</h1>;
+    const { register, control, watch } = useForm({
+        defaultValues: {
+            contas_bancarias: [
+                {
+                    nomeBanco: "",
+                    numeroConta: "",
+                    agencia: "",
+                    isPix: false,
+                    pix: {},
+                },
+            ],
+        },
+    });
+    const { fields, append, remove } = useFieldArray({
+        name: "contas_bancarias",
+        control,
+    });
+    const { addNewData } = useContext(cadastroContext);
+    const Bancos = [
+        "",
+        "C6 Bank",
+        "Banco do Brasil",
+        "Bradesco",
+        "Caixa",
+        "Itaú",
+        "Nubank",
+        "Santander",
+        "Outro",
+    ];
+    const TipoPix = [
+        "",
+        "CPF",
+        "CNPJ",
+        "E-mail",
+        "Telefone",
+        "chave aleatoria",
+    ];
+    console.log(watch("contas_bancarias"));
+    return (
+        <form onSubmit={(data) => console.log(data)} class="mb-3">
+            <button
+                onClick={(e) => {
+                    e.preventDefault();
+                    append({});
+                }}
+            >
+                add new bank
+            </button>
+            {fields.map((item, index) => {
+                return (
+                    <div key={item.id}>
+                        <label htmlFor="">
+                            <span>nomeConta</span>
+                            <select
+                                {...register(
+                                    `contas_bancarias.${index}.nomeBanco`
+                                )}
+                                class="form-select"
+                            >
+                                {Bancos.map((item, index) => {
+                                    return (
+                                        <option value={item} key={index}>
+                                            {item}
+                                        </option>
+                                    );
+                                })}
+                            </select>
+                        </label>
+                        <label htmlFor="">
+                            <span>numeroConta</span>
+                            <input
+                                class="form-control form-control-lg"
+                                type="text"
+                                {...register(
+                                    `contas_bancarias.${index}.numeroConta`
+                                )}
+                                id=""
+                            />
+                        </label>
+                        <label htmlFor="">
+                            <span>agencia</span>
+                            <input
+                                class="form-control form-control-lg"
+                                type="text"
+                                {...register(
+                                    `contas_bancarias.${index}.agencia`
+                                )}
+                                id=""
+                            />
+                        </label>
+                        <input
+                            class="form-check-input mt-0"
+                            type="checkbox"
+                            id=""
+                            {...register(`contas_bancarias.${index}.isPix`)}
+                        />
+                        <button
+                            onClick={(e) => {
+                                e.preventDefault();
+                                remove(index);
+                            }}
+                        >
+                            apagar
+                        </button>
+                        {watch(`contas_bancarias.${index}.isPix`) && (
+                            <div>
+                                tipo Pix
+                                <select
+                                    {...register(
+                                        `contas_bancarias.${index}.pix.tipo`
+                                    )}
+                                    class="form-select"
+                                >
+                                    {TipoPix.map((item, index) => {
+                                        return (
+                                            <option value={item} key={index}>
+                                                {item}
+                                            </option>
+                                        );
+                                    })}
+                                </select>
+                            </div>
+                        )}
+                    </div>
+                );
+            })}
+            <button
+                onClick={(e) => {
+                    e.preventDefault();
+                    addNewData(watch("contas_bancarias"));
+                }}
+            >
+                os dados estão corretos ?
+            </button>
+        </form>
+    );
 }
 
 export default Conta;
