a924398a3ac8d76fafc921fd792b3f9fe9e12dee
Lucas Bispo <skaliboo1@gmail.com>
2022-11-14 19:09:45 -0300

    css

--- Frontend/front/src/Pages/Juridica.jsx
+++ Frontend/front/src/Pages/Juridica.jsx
@@ -2,32 +2,53 @@ import React from "react";
 
 function Juridica(props) {
     return (
-        <>
-            <label htmlFor="">
-                Nome Fantasia
-                <input
-                    type="text"
-                    {...props.p("nomeFantasia", { required: false, value: "" })}
-                />
-            </label>
-            <label htmlFor="">
-                Ramo
-                <input
-                    type="text"
-                    {...props.p("ramo", { required: false, value: "l" })}
-                />
-            </label>
-            <label htmlFor="">
-                Inscrição Estadual
-                <input
-                    type="text"
-                    {...props.p("inscricaoEstadual", {
-                        required: false,
-                        value: "",
-                    })}
-                />
-            </label>
-        </>
+        <div class="row g-2">
+            <div class="col-6">
+                <div class="input-group">
+                    <label htmlFor="">
+                        Nome Fantasia
+                        <input
+                            type="text"
+                            className="form-control"
+                            {...props.p("nomeFantasia", {
+                                required: false,
+                                value: "",
+                            })}
+                        />
+                    </label>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="input-group">
+                    <label htmlFor="">
+                        Ramo
+                        <input
+                            type="text"
+                            className="form-control"
+                            {...props.p("ramo", {
+                                required: false,
+                                value: "l",
+                            })}
+                        />
+                    </label>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="input-group">
+                    <label htmlFor="">
+                        Inscrição Estadual
+                        <input
+                        className="form-control"
+                            type="text"
+                            {...props.p("inscricaoEstadual", {
+                                required: false,
+                                value: "",
+                            })}
+                        />
+                    </label>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- Frontend/front/src/Pages/pessoa.jsx
+++ Frontend/front/src/Pages/pessoa.jsx
@@ -25,118 +25,179 @@ function Pessoa() {
 
     return (
         <form method="post" onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="">
-                Nome Completo
-                <input
-                    type="text"
-                    {...register("nomeCompleto", {
-                        required: "Nome completo é obrigatório",
-                    })}
-                />
-                <span>{errors.nomeCompleto?.message}</span>
-            </label>
-            <label htmlFor="">
-                Data de Nascimento
-                <input
-                    type="date"
-                    {...register("dataDeNascimento", {
-                        required: "Data de nascimento é obrigatório",
-                    })}
-                />
-                <span>{errors.dataDeNascimento?.message}</span>
-            </label>
-            <label htmlFor="">
-                Sexo
-                <select
-                    name=""
-                    id=""
-                    {...register("sexo", { required: "Sexo é obrigatório" })}
-                >
-                    <option value=""></option>
-                    <option value="Homem">Homem</option>
-                    <option value="Mulher">Mulher</option>
-                    <option value="Outros"> Prefiro Não dizer</option>
-                </select>
-                <span>{errors.sexo?.message}</span>
-            </label>
-            <label htmlFor="">
-                Estado Civil
-                <select
-                    name=""
-                    id=""
-                    {...register("estadoCivil", {
-                        required: "Estado civil é obrigatório",
-                    })}
-                >
-                    <option value=""></option>
-                    <option value="Solteiro(a)">Solteiro(a)</option>
-                    <option value="Casado(a)">Casado(a)</option>
-                    <option value="Divorciado(a)">Divorciado(a)</option>
-                    <option value="Viúvo(a)">Viúvo(a)</option>
-                </select>
-                <span>{errors.estadoCivil?.message}</span>
-            </label>
-            <label htmlFor="">
-                Nacionalidade
-                <input
-                    type="text"
-                    {...register("nacionalidade", {
-                        required: "Nacionalidade é obrigatório",
-                    })}
-                />
-                <span>{errors.nacionalidade?.message}</span>
-            </label>
-            <label htmlFor="">
-                Naturalidade
-                <input
-                    type="text"
-                    {...register("naturalidade", {
-                        required: "Naturalidade é obrigatório",
-                    })}
-                />
-                <span>{errors.naturalidade?.message}</span>
-            </label>
-            <label htmlFor="">
-                Situação
-                <select
-                    name=""
-                    id=""
-                    {...register("situação", {
-                        required: "Situação é obrigatório",
-                    })}
-                >
-                    <option value=""></option>
-                    <option value="Regular">Regular</option>
-                    <option value="Irregular">Iregular</option>
-                </select>
-                <span>{errors.situação?.message}</span>
-            </label>
-            <label htmlFor="">
-                CPF / CNPJ
-                <input
-                    type="text"
-                    {...register("cpfcnpj", {
-                        required: "CPF/CNPJ é obrigatório",
-                    })}
-                />
-                <span>{errors.cpfcnpj?.message}</span>
-            </label>
-            <label htmlFor="">
-                é Pessoa Física?
-                <input type="checkbox" {...register("isJuridic")} id="" />
-            </label>
-            {!watch("isJuridic") ? (
-                <label htmlFor="">
-                    seus Dados estão corretos ?<button>Sim, Estão!</button>
-                </label>
-            ) : (
-                <>
-                    <Juridica p={register} />
+            <div class="row g-2">
+                <div class="col-6">
+                    <div class="input-group">
+                        <label htmlFor="">
+                            Nome Completo
+                            <input
+                                class="form-control"
+                                type="text"
+                                {...register("nomeCompleto", {
+                                    required: "Nome completo é obrigatório",
+                                })}
+                            />
+                            <span>{errors.nomeCompleto?.message}</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="input-group">
+                        <label htmlFor="">
+                            Data de Nascimento
+                            <input
+                                class="form-control"
+                                type="date"
+                                {...register("dataDeNascimento", {
+                                    required:
+                                        "Data de nascimento é obrigatório",
+                                })}
+                            />
+                            <span>{errors.dataDeNascimento?.message}</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="input-group">
+                        <label htmlFor="">
+                            Sexo
+                            <select
+                                name=""
+                                id=""
+                                class="form-control form-select"
+                                {...register("sexo", {
+                                    required: "Sexo é obrigatório",
+                                })}
+                            >
+                                <option value=""></option>
+                                <option value="Homem">Homem</option>
+                                <option value="Mulher">Mulher</option>
+                                <option value="Outros">
+                                    {" "}
+                                    Prefiro Não dizer
+                                </option>
+                            </select>
+                            <span>{errors.sexo?.message}</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="input-group">
+                        <label htmlFor="">
+                            Estado Civil
+                            <select
+                                class="form-control form-select"
+                                name=""
+                                id=""
+                                {...register("estadoCivil", {
+                                    required: "Estado civil é obrigatório",
+                                })}
+                            >
+                                <option value=""></option>
+                                <option value="Solteiro(a)">Solteiro(a)</option>
+                                <option value="Casado(a)">Casado(a)</option>
+                                <option value="Divorciado(a)">
+                                    Divorciado(a)
+                                </option>
+                                <option value="Viúvo(a)">Viúvo(a)</option>
+                            </select>
+                            <span>{errors.estadoCivil?.message}</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="input-group">
+                        <label htmlFor="">
+                            Nacionalidade
+                            <input
+                                type="text"
+                                className="form-control"
+                                {...register("nacionalidade", {
+                                    required: "Nacionalidade é obrigatório",
+                                })}
+                            />
+                            <span>{errors.nacionalidade?.message}</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="input-group">
+                        <label htmlFor="">
+                            Naturalidade
+                            <input
+                                type="text"
+                                className="form-control"
+                                {...register("naturalidade", {
+                                    required: "Naturalidade é obrigatório",
+                                })}
+                            />
+                            <span>{errors.naturalidade?.message}</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="input-group">
+                        <label htmlFor="">
+                            Situação
+                            <select
+                                name=""
+                                class="form-control form-select"
+                                id=""
+                                {...register("situação", {
+                                    required: "Situação é obrigatório",
+                                })}
+                            >
+                                <option value=""></option>
+                                <option value="Regular">Regular</option>
+                                <option value="Irregular">Iregular</option>
+                            </select>
+                            <span>{errors.situação?.message}</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="input-group">
+                        <label htmlFor="">
+                            CPF / CNPJ
+                            <input
+                                type="text"
+                                className="form-control"
+                                {...register("cpfcnpj", {
+                                    required: "CPF/CNPJ é obrigatório",
+                                })}
+                            />
+                            <span>{errors.cpfcnpj?.message}</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="input-group">
+                        <label htmlFor="">
+                            é Pessoa Física?
+                            <input
+                                type="checkbox"
+                                {...register("isJuridic")}
+                                id=""
+                            />
+                        </label>
+                    </div>
+                </div>
+                {!watch("isJuridic") ? (
                     <label htmlFor="">
                         seus Dados estão corretos ?<button>Sim, Estão!</button>
                     </label>
-                </>
-            )}
+                ) : (
+                    <>
+                        <Juridica p={register} />
+                        <label htmlFor="">
+                            seus Dados estão corretos ?
+                            <button className="btn btn-primary">
+                                Sim, Estão!
+                            </button>
+                        </label>
+                    </>
+                )}
+            </div>
         </form>
     );
 }
