6a626bb40a40aabe308a085deed02a80078663f5
Lucas Bispo <skaliboo1@gmail.com>
2022-11-18 18:24:34 -0300

    create interface

---
+++ backend/src/interfaces/index.ts
@@ -0,0 +1,14 @@
+export interface IcreatedJuridic {
+		isJuridic?: boolean;
+		ramo: string;
+		nomeCompleto: string;
+		dataDeNascimento: string;
+		sexo: string;
+		estadoCivil: string;
+		nacionalidade: string;
+		naturalidade: string;
+		situação: string;
+		cpfcnpj: string;
+		nomeFantasia: string;
+		inscricaoEstadual: string;
+}
