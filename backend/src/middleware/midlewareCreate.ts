5d978660a0e32a4de59210bf2d64278191c4c91e
Lucas Bispo <skaliboo1@gmail.com>
2022-11-20 02:44:31 -0300

    adiciona manipulador de erro

--- backend/src/middleware/midlewareCreate.ts
+++ backend/src/middleware/midlewareCreate.ts
@@ -10,15 +10,18 @@ import {
 	schemaEndereco,
 	schemaTelefone,
 	schemaConta,
-	schemaUsuario
+	schemaUsuario,
+	schemaDocs,
 } from "../validations/schema/created";
-import 'express-async-errors'
+import "express-async-errors";
 
 export const midCreated = (req: Request, res: Response, next: NextFunction) => {
 	const { error } = schemaPessoa.validate(req.body.pessoa);
 	const { error: errorUser } = schemaUsuario.validate(req.body.usuario);
+	const { error: errorDocs } = schemaDocs.validate(req.body.docs);
 	if (error) throw new ErrorApi(error.message, 400);
 	if (errorUser) throw new ErrorApi(errorUser.message, 400);
+	if (errorDocs) throw new ErrorApi(errorDocs.message, 400);
 	pixInsertKey(req, removerKeyPix);
 	addMainInRequest(req, "endereco", schemaEndereco);
 	addMainInRequest(req, "telefone", schemaTelefone);
