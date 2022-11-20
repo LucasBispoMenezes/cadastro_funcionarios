ba2cbf1e2b698b5c0cb7174c5b2938aced5b452e
Lucas Bispo <skaliboo1@gmail.com>
2022-11-20 02:46:12 -0300

    refatura função de requisição

--- Frontend/front/src/helpers/requests.js
+++ Frontend/front/src/helpers/requests.js
@@ -3,11 +3,15 @@ import { getToken } from './sessionStorage.js'
 
 const instance = axios.create({
     baseURL: 'http://localhost:3001/',
-    timeout: 2000
 })
 
 export const requestLogin = async (url, body) => {
-    return await instance.post(url, body)
+    try {
+        return await instance.post(url, body)
+    } catch (error) {
+        return await error.response.data.message
+
+    }
 }
 
 export const getUser = async (url) => {
