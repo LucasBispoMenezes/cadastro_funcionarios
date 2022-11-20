59a82b0424dfc3bf9274eaf67b95d97ca2019c93
Lucas Bispo <skaliboo1@gmail.com>
2022-11-19 23:56:42 -0300

    adiciona validação no middleware de erro

--- backend/src/middleware/midlewareCreate.ts
+++ backend/src/middleware/midlewareCreate.ts
@@ -1,10 +1,27 @@
 import { NextFunction, Request, Response } from "express";
-import { addMainInRequest, pixInsertKey, removerKeyPix } from '../utils/manipulationReq'
+import { ErrorApi } from "../classes/classeError";
+import {
+	addMainInRequest,
+	pixInsertKey,
+	removerKeyPix,
+} from "../utils/manipulationReq";
+import {
+	schemaPessoa,
+	schemaEndereco,
+	schemaTelefone,
+	schemaConta,
+	schemaUsuario
+} from "../validations/schema/created";
+import 'express-async-errors'
+
 export const midCreated = (req: Request, res: Response, next: NextFunction) => {
+	const { error } = schemaPessoa.validate(req.body.pessoa);
+	const { error: errorUser } = schemaUsuario.validate(req.body.usuario);
+	if (error) throw new ErrorApi(error.message, 400);
+	if (errorUser) throw new ErrorApi(errorUser.message, 400);
 	pixInsertKey(req, removerKeyPix);
-	addMainInRequest(req, "endereco");
-	addMainInRequest(req, "telefone");
-	addMainInRequest(req, "contas_bancarias");
-	removerKeyPix(req);
+	addMainInRequest(req, "endereco", schemaEndereco);
+	addMainInRequest(req, "telefone", schemaTelefone);
+	addMainInRequest(req, "contas_bancarias", schemaConta);
 	next();
 };
